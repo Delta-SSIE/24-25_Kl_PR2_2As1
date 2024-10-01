c30ea54b95d070fd122a619327cbd263a587f048
Jan Koupil <jan.koupil@gmail.com>
2024-10-01 13:53:00 +0200

    2024-10-01 Lanovka jako linked list

---
+++ 00-Rev-posledni-dlouhe/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace _00_Rev_posledni_dlouhe
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] slova = { "Karel", "Eva", "Jaromír", "Iva", "Martin" };
+            Console.WriteLine(PosledniDlouhe(slova, 5)); //vypíše "Martin"
+            Console.WriteLine(PosledniDlouhe(slova, 7)); //vypíše "Jaromír"
+            Console.WriteLine(PosledniDlouhe(slova, 12)); //vypíše prázdno
+            Console.WriteLine(PosledniDlouhe(new string[0], 5));
+        }
+
+        private static string PosledniDlouhe(string[] slova, int limit)
+        {
+            // Najít poslední řetězec, který splňuje podmínku délky
+            return slova.LastOrDefault(s => s.Length >= limit) ?? "";
+        }
+    }
+}
