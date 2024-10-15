ad81ace7e0b71656eb574e98ca7de69390dacee0
Jan Koupil <jan.koupil@gmail.com>
2024-10-15 08:23:25 +0200

    Ukazkova reseni Loop, Paginator, LinkedList

---
+++ 01-OOP-060-Paginator/Paginator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_060_Paginator
+{
+    internal class Paginator
+    {
+        private int pageSize;
+        private string[] items;
+        public int ItemCount => items.Length;        
+
+        public int PageCount
+        {
+            get
+            {
+                if (ItemCount % pageSize == 0)
+                    return ItemCount / pageSize;
+                else
+                    return ItemCount / pageSize + 1; //to když poslední stránka bude neúplná
+            }
+        }
+
+        public Paginator(string[] items, int pageSize)
+        {
+            if (pageSize < 1)
+                throw new ArgumentOutOfRangeException();
+
+            this.pageSize = pageSize;
+            this.items = items;
+        }
+
+        public int GetPageItemCount(int n)
+        {
+            if (n < 0 || n >= PageCount)
+                return 0;
+            else if (n < PageCount - 1)
+                return pageSize;
+            else
+                return ItemCount % pageSize;
+        }
+
+        public string[] GetPage(int n)
+        {
+            if (n < 0 || n > PageCount)
+                return new string[0];
+
+            int limit = Math.Min(pageSize * (n + 1), ItemCount);
+
+            List<string> pageItems = new List<string>();
+
+            for (int i = pageSize * n; i < limit; i++)
+            {
+                pageItems.Add(items[i]);
+            }
+            //nebo lze použít v novějších verzích C# range operátor ..  ([7..14])
+
+            return pageItems.ToArray();
+        }
+
+        public int FindPage(string s)
+        {
+            int pozice = Array.IndexOf(items, s);
+
+            if (pozice < 0)
+                return -1;
+            else if (pozice % pageSize == 0)
+                return pozice / pageSize + 1;
+            else
+                return pozice / pageSize + 2;
+
+        }
+
+    }
+}
---
+++ 01-OOP-070-Loop/Loop.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_070_Loop
+{
+    internal class Loop
+    {
+        private int[] items;
+        private int index;
+
+        public int Current
+        {
+            get => items[index];            
+            set { items[index] = value; }
+        }
+
+        public Loop(int[] items)
+        {
+            this.items = items; //chtělo by lépe, abych udělal kopii
+            index = 0;
+        }
+
+        public void Right()
+        {
+            index++;
+            if (index >= items.Length)
+                index = 0;
+        }
+
+        public void Right(int count)
+        {
+            for (int i = 0; i < count; i++)
+                Right();
+        }
+
+        //public void Right(int count)
+        //{
+        //    index += count;
+        //    while (index >= items.Length) //a tohle bych správně měl dělat jako modulus %
+        //        index -= items.Length;
+        //}
+
+        public void Left()
+        {
+            index--;
+            if (index < 0)
+                index = items.Length - 1;
+        }
+
+        public void Left(int count)
+        {
+            index -= count;
+            while (index < 0)
+                index += items.Length;
+        }
+    }
+}
