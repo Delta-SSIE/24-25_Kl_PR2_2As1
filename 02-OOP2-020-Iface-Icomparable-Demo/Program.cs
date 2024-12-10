b7132621a1b7169c5eb48c2fb5817a80c6aed2f2
KOUPIL Jan <koupja@delta-skola.cz>
2024-12-10 09:27:43 +0100

    2024-12-10 IComaparable demo

--- 02-OOP2-020-Iface-Icomparable-Demo/Program.cs
+++ 02-OOP2-020-Iface-Icomparable-Demo/Program.cs
@@ -26,13 +26,51 @@
             {
                 Console.WriteLine($"{strom.Druh}: {strom.Vyska}");
             }
+
+            Strom bobrovice = new Strom() { Druh = "Borovice", Vyska = 15 };
         }
     }
 
-    class Strom
+    class Strom : IComparable
     {
         public string Druh { get; set; }
         public double Vyska { get; set; }
+
+
+        //public int CompareTo(object? obj)
+        //{
+        //    return Vyska.CompareTo(((Strom)obj).Vyska); //použiju nativní porovnání podle double
+        //}
+
+        int IComparable.CompareTo(object? obj)
+        {
+            if (obj != null && obj is Strom druhyStrom)
+            {
+                if (druhyStrom.Vyska > this.Vyska)
+                    return -1;
+                else if (druhyStrom.Vyska < this.Vyska)
+                    return 1;
+                else
+                    return 0;
+            }
+            else
+            {
+                throw new Exception("Cannot compare - invalid type");
+            }
+        }
+
+        //public int CompareTo(object? obj)
+        //{
+        //    if (obj != null && obj is Strom druhyStrom)
+        //    {
+        //        double rozdil = this.Vyska - druhyStrom.Vyska;
+        //        return Math.Sign(rozdil);
+        //    }
+        //    else
+        //    {
+        //        throw new Exception("Cannot compare - invalid type");
+        //    }
+        //}
     }
     
 }
