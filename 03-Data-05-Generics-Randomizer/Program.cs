5b9e83f579dbab99cf3bf5faf8f3bf856f72e6bf
KOUPIL Jan <koupja@delta-skola.cz>
2025-02-18 09:25:05 +0100

    2025-02-18 Randomizer int

---
+++ 03-Data-05-Generics-Randomizer/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _03_Data_05_Generics_Randomizer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = [1, 2, 3, 4, 5, 6, 7];
+            Randomizer randomizer = new(nums);
+            while (randomizer.Remaining > 0)
+            {
+                Console.WriteLine($"Další číslo je {randomizer.Next()}");
+            }
+        }
+    }
+}
