0fe884385cc177b5c6b59dcfb3b1d3fd83fed707
KOUPIL Jan <koupja@delta-skola.cz>
2025-02-18 09:36:58 +0100

    2025-02-18 Generic randomizer

--- 03-Data-05-Generics-Randomizer/Program.cs
+++ 03-Data-05-Generics-Randomizer/Program.cs
@@ -5,10 +5,19 @@
         static void Main(string[] args)
         {
             int[] nums = [1, 2, 3, 4, 5, 6, 7];
-            Randomizer randomizer = new(nums);
+            GenericRandomizer<int> randomizer = new(nums);
             while (randomizer.Remaining > 0)
             {
                 Console.WriteLine($"Další číslo je {randomizer.Next()}");
+            }
+
+            Console.WriteLine();
+
+            string[] names = ["Anna", "Marie", "Kateřina", "Eliška"];
+            GenericRandomizer<string> randomizer2 = new(names);
+            while (randomizer2.Remaining > 0)
+            {
+                Console.WriteLine($"Další jméno je {randomizer2.Next()}");
             }
         }
     }
