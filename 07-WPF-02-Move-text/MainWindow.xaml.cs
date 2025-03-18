716cd9a381b744d0de140781caaf9d26498cf9b3
KOUPIL Jan <koupja@delta-skola.cz>
2025-03-18 17:10:03 +0100

    2025-03-18
    WPF - úvod

---
+++ 07-WPF-02-Move-text/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _07_WPF_02_Move_text;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void moveBtn_Click(object sender, RoutedEventArgs e)
+    {
+        MoveTextAndFocus();
+    }
+
+    private void MoveTextAndFocus()
+    {
+        outputTB.Text = inputTB.Text;
+        inputTB.Text = string.Empty;
+        inputTB.Focus();
+    }
+
+    private void inputTB_KeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+            MoveTextAndFocus();
+
+    }
+}
---
+++ 07-WPF-03-Add-10/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _07_WPF_03_Add_10;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void inputTB_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (outputTB == null)
+            return;
+
+        if (double.TryParse(inputTB.Text, out double inputNum))
+        { 
+            double outputNum = inputNum + 10;
+            outputTB.Text = outputNum.ToString();
+
+            outputTB.Background = Brushes.Transparent;
+            outputTB.Foreground = Brushes.Black;
+            outputTB.FontWeight = FontWeights.Normal;
+
+        }
+        else
+        {
+            outputTB.Text = "NaN";
+            outputTB.Background = Brushes.Red;
+            outputTB.Foreground = Brushes.White;
+            outputTB.FontWeight = FontWeights.Bold;
+        }
+    }
+}
